ec89fd11635f35494e0e79bb940e585e2d26fb64
oscarBV <oscarBV@users.noreply.github.com>
2014-08-29 00:13:19 +0200

    Canvis al debug

--- blue_led_fix_gnss/blue_led_fix_gnss.ino
+++ blue_led_fix_gnss/blue_led_fix_gnss.ino
@@ -1,4 +1,20 @@
-#define debug 1
+/*
+
+ Blue_Led_Fix_GNSS 
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ 
+ github URL :-
+ https://github.com/oscarBV/NavSpark/tree/master/blue_led_fix_gnss
+ 
+ Després de fer el HELLO WORLD el led blau no feia blink!!!!
+ 
+*/
+
+// Comentar per treure els miisatges de debug
+// #define DEBUG
 
 uint8_t gpio_bit = 0;
 
@@ -14,16 +30,20 @@ void loop()
 
 void task_called_after_GNSS_update(void)
 {
-static uint8_t val = 0;  
-char buf[128];
-int len;
+#ifdef DEBUG
+  char buf[128];
+  int len;
+#endif
 U08 fixmode;
-
-
+static uint8_t val = 0;  
+  
 GnssInfo.update();
 fixmode = GnssInfo.fixMode(); 
-len = sprintf(buf, "fix mode: %3d\r\n", fixmode);
-gnss_uart_putline(0, (U08*) buf, len);
+
+#ifdef DEBUG
+  len = sprintf(buf, "fix mode: %3d\r\n", fixmode);
+  gnss_uart_putline(0, (U08*) buf, len);
+#endif
 
 if (fixmode >= 1) {
   if (val == 1) gnss_gpio_high(gpio_bit);
